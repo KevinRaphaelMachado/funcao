a6db44141d16aeaa286656cc82e28d046f2c9968
KevinRaphaelMachado <machadokevin100@gmail.com>
2024-07-01 21:12:44 -0300

    finalizado usando funcao ler numero do maior para o menor

--- maiorNum.js
+++ maiorNum.js
@@ -1,8 +1,32 @@
-const prompt = requere("prompt-sync")();
-
-    let imput = prompt('adicione um numero')
-    function maiorNum(num ) {
-        let array = num.split("")
-    }
 
     
+
+    const prompt = require("prompt-sync")();
+
+function ehNumero(num) {
+    return !isNaN(Number(num));
+}
+
+function maiorNumero(num) {
+    if (ehNumero(num)) {
+    let array = num.split("");
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+        if (array[i] < array[j]) {
+            let aux = array[j];
+            array[j] = array[i];
+            array[i] = aux;
+        }
+    }
+}
+
+    return array.join("");
+}
+    console.log("Número inválido");
+}
+
+const numero = prompt("Digite um número: ");
+
+let maior = maiorNumero(numero);
+console.log(maior);
